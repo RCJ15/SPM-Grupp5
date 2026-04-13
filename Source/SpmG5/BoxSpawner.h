39a622880382098bfe25fcee715bc1abff92c589
Viktor R-Soop <duddesoop@gmail.com>
2026-04-13 11:38:31 +0200

    Created box spawner calss and basic stuckture

---
+++ Source/SpmG5/BoxSpawner.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BoxSpawner.generated.h"
+
+UCLASS()
+class SPMG5_API ABoxSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABoxSpawner();
+	void SpawnBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+	UPROPERTY(EditAnywhere)
+	FVector SpawnLocation;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> BoxToSpawn;
+
+
+};
