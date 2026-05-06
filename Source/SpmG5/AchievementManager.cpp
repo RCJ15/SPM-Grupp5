96dedea5e2454160770e3b0e60c244c09e322837
Arex <56590254+Arex2@users.noreply.github.com>
2026-05-06 22:49:01 +0200

    Achievements pt.2

--- Source/SpmG5/AchievementManager.cpp
+++ Source/SpmG5/AchievementManager.cpp
@@ -5,37 +5,74 @@
 
 AchievementManager::AchievementManager()
 {
-	
+	LoadAchievements();
+	//kolla igenom array, om !bIsUnlocked
 	//subscribea olika achievement metoder på händelser
-	//TryUnlockAchievement1()
 }
 
 AchievementManager::~AchievementManager()
 {
 	//innan destruct spara??
+	SaveAchievements();
+}
+
+//metod för att ladda in achievements från en fil, returnar success eller inte
+bool AchievementManager::LoadAchievements()
+{
+	//från filen börja med att läsa antal achievements
+	//skapa Achievements arrayen utifrån detta antalet
+	//och läs sedan varje achievement del från filen
+	//och skapa dessa till FAchievement
+	//som man lägger in i Achievements arrayen
+	return false;
+}
+
+//metod för att spara ner achievements till en fil, returnar success eller inte
+bool AchievementManager::SaveAchievements()
+{
+	//i fil skriv ned
+	//antal achievements
+	//och sen lista varje achievement med dess delar
+	
+	return false;
+}
+
+void AchievementManager::IncreaseAchievementCount(int const AchievementIndex)
+{
+	FAchievement A = Achievements[AchievementIndex];
+	A.CurrentEventCount++;
+	if (A.CurrentEventCount >= A.ToReachEventCount)
+	{
+		UnlockAchievement(AchievementIndex);
+	}
+}
+
+void AchievementManager::UnlockAchievement(int const AchievementIndex)
+{
+	Achievements[AchievementIndex].bIsUnlocked = true;
+	//unsubscribe dens korresponderande metod från stuff
+	//så behöver skicka med metod typ...
+}
+
+void AchievementManager::CheckFulfillAchievement0()
+{
+	//kolla om krav uppfylls
+	
+	
+	//om ja
+	IncreaseAchievementCount(0);
 }
 
 void AchievementManager::CheckFulfillAchievement1()
 {
-	//kolla om krav uppfylls
-	SortedBoxes++;
-	
-	if (SortedBoxes >= 10)
-	{
-		UnlockAchievement(A1);
-	}
 }
 
-bool AchievementManager::UnlockAchievement(Achievement A)
+void AchievementManager::CheckFulfillAchievement2()
 {
-	//kolla om redan unlocked, annars unlock
-	//borde finnas ett bättre sätt att göra detta som unsubscibear detta achievements metod
-	//if (UnlockedAchievements.Contains(A))
-		//return false;
-	
-	UnlockedAchievements.Add(A);
-	return true;
-	
+}
+
+void AchievementManager::CheckFulfillAchievement3()
+{
 }
 
 
