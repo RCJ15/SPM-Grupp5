3076bafc9f66921e2f241db20e39a6eb4396ed2b
Viktor R-Soop <duddesoop@gmail.com>
2026-06-02 15:28:11 +0200

    Scene fixes and bomb force when ragdolling

--- Source/SpmG5/Bomb.cpp
+++ Source/SpmG5/Bomb.cpp
@@ -63,6 +63,7 @@ void ABomb::Explode()
 			ASpmG5Character* Character = Cast<ASpmG5Character>(i.GetActor());
 			//spelaren måste droppa item
 			Character->Drop();
+			Character->AddVelocity((GetActorLocation() - Character->GetActorLocation())/GetActorLocation()*10);
 			Character->DoRagdoll();
 		}
 		
