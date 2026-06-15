10525929558fe06769057230c6158e2c79770d98
Arex <56590254+Arex2@users.noreply.github.com>
2026-06-15 12:54:13 +0200

    .

--- Source/SpmG5/SpmG5Character.cpp
+++ Source/SpmG5/SpmG5Character.cpp
@@ -468,6 +468,10 @@ void ASpmG5Character::StartFindingValidPosition()
 	GetWorld()->GetTimerManager().SetTimer(FindValidPosTimer, this, &ASpmG5Character::FindValidPosition, 0.2, true, 0.2);
 }
 
+void ASpmG5Character::FindValidPosition()
+{
+}
+
 
 void ASpmG5Character::DoMove(float Right, float Forward)
 {
